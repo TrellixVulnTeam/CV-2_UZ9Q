e2e74ea84947d276edaa81a8d793d8b17584a2e0
Weronika <laskowska29@gmail.com>
2021-08-25 21:20:08 +0200

    some addings

--- dist/js/app.js
+++ dist/js/app.js
@@ -24,4 +24,24 @@ window.addEventListener('scroll', function () {
         menu.style.top="70px";
     }
     lastScrollTop=scrollTop;
-})
+});
+
+window.addEventListener('scroll', reveal);
+
+function reveal () {
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i=0; i<reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        if(revealTop<windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+    }
+}
+let revealsInstant= document.querySelectorAll('.reveal-instant')
+window.onload= () => {
+    for(var i=0; i<revealsInstant.length; i++){
+        revealsInstant[i].classList.add('active');
+    }
+}
